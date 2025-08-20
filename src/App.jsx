83d3342433721f9f1be598fada59b85d6d1b8ca7
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-08-20 09:30:28 -0300

    feat: add Header component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-     <div className=''>
+     <div className='container-fluid py-4'> 
+      <div className='row justify-content-center'>
+        <div className='col-12 col-xl-10'>
+
+        </div>
+      </div>
 
      </div>
     </>
